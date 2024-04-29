2208e3ad410e4a08120bade6611845c9f6fbb6fa
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-04-28 23:15:18 -0400

    Added atoms to change the items in the cart like add, remove, increment, decrement and find index of the item

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,13 @@ import { CartItem } from "../types";
 
 // The atom, `cartItemsAtom`, is initialized using `atomWithStorage` from `jotai/utils`, which is a utility function provided by Jotai for persisting state in storage (like `localStorage` or `sessionStorage`). This atom is specifically for storing an array of `CartItem` objects, representing the items in a shopping cart. 
 
-/* Key: `"cartItems"` is the key under which the array is stored in the storage.
- Default Value: `[]` indicates that the default value is an empty array. If there is no existing data in storage under `"cartItems"`, this default value is used. */
+/* 
+Key: `"cartItems"` is the key under which the array is stored in the storage.
+Default Value: `[]` indicates that the default value is an empty array. If there is no existing data in storage under `"cartItems"`, this default value is used. 
+
+When using atomWithStorage, the data will be persisted in your localStorage (or sessionStorage if you prefer) — it will be used to seed the atoms on subsequent requests.
+
+ */
 export const cartItemsAtom = atomWithStorage<CartItem[]>("cartItems", []);
 
 /* 
@@ -28,3 +33,57 @@ export const cartTotalAtom = atom((get) => {
       return total + item.product.price * item.quantity;
     }, 0);
 });
+
+
+// Write only atom = this is two way data binding
+// Be careful with this pattern, you may sometimes just do the logic in the component, or in the atom setter function (read/write atom)
+export const addItemAtom = atom(null, (get, set, item: CartItem) => {
+    const items = get(cartItemsAtom);
+    const itemIndex = findItemIndex(items, item);
+    
+    if (itemIndex === -1) {
+    set(cartItemsAtom, [...items, { ...item, quantity: 1 }]);
+    }
+});
+    
+export const removeItemAtom = atom(null, (get, set, item: CartItem) => {
+    const items = get(cartItemsAtom);
+    const itemIndex = findItemIndex(items, item);
+
+    if (itemIndex > -1) {
+    items.splice(itemIndex, 1);
+    set(cartItemsAtom, [...items]);
+        }
+});
+    
+export const incrementQuantityAtom = atom(null, (get, set, item: CartItem) => {
+    const items = get(cartItemsAtom);
+    const itemIndex = findItemIndex(items, item);
+
+    if (itemIndex > -1) {
+    items[itemIndex].quantity++;
+    set(cartItemsAtom, [...items]);
+    }
+});
+    
+export const decrementQuantityAtom = atom(null, (get, set, item: CartItem) => {
+    const items = get(cartItemsAtom);
+    const itemIndex = findItemIndex(items, item);
+
+    if (itemIndex > -1) {
+    items[itemIndex].quantity--;
+
+    // if the decremented item quantity is 0, remove the item
+    if (items[itemIndex].quantity === 0) {
+        items.splice(itemIndex, 1);
+    }
+
+    set(cartItemsAtom, [...items]);
+    }
+});
+    
+const findItemIndex = (cartItems: CartItem[], item: CartItem) => {
+    return cartItems.findIndex(
+    (cartItem) => cartItem.product.id === item.product.id
+    );
+};
